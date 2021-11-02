5d0d34cc79deafb255a9156dd8330eeb0b8ebca1
Giovane Aires Gomes <giovaneag17@outlook.com>
2021-11-02 20:25:41 -0300

    Finalização da primeira aula de introdução ao react

--- introducao-reactjs/src/index.js
+++ introducao-reactjs/src/index.js
@@ -1,35 +1,71 @@
-import React from "react"
+import React, { Component } from "react"
 import ReactDOM from "react-dom"
-import Button from './Button'
-import ComponentA from './ComponentA'
-import ComponentB from './ComponentB'
+// import Button from './Button'
+// import ComponentA from './ComponentA'
+// import ComponentB from './ComponentB'
 import "./styles.css"
 
-function sum(a, b) {
-    alert(a + b)
-}
+// function sum(a, b) {
+//     alert(a + b)
+// }
 
-function firstJSX() {
-    return (
-        <div className="test">
-            Giovane Aires - Introdução ao ReactJS
-        </div>
-    )
-}
+// function firstJSX() {
+//     return (
+//         <div className="test">
+//             Giovane Aires - Introdução ao ReactJS
+//         </div>
+//     )
+// }
 
-const App = () => {
+// const App = () => {
 
-    return (
-        <div className="App">
-            { firstJSX() }
-            <Button onClick={() => sum(11,6)} name={'Giovane Aires'}/>
-            <ComponentA>
-                <ComponentB>
-                    <Button onClick={() => sum(5,6)} name={'João Aires'}/>
-                </ComponentB>
-            </ComponentA>
-        </div>
-    )
+//     return (
+//         <div className="App">
+//             { firstJSX() }
+//             <Button onClick={() => sum(11,6)} name={'Giovane Aires'}/>
+//             <ComponentA>
+//                 <ComponentB>
+//                     <Button onClick={() => sum(5,6)} name={'João Aires'}/>
+//                 </ComponentB>
+//             </ComponentA>
+//         </div>
+//     )
+// }
+
+class App extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            clock: 1000,
+            cup: 'water'
+        }
+    }
+
+    componentDidMount() {
+        window.setTimeout(() => {
+            this.setState({
+                cup: 'juice'
+            })
+        }, 3000)
+    }
+    changeCup = () => {
+        this.setState({
+            cup: 'soda'
+        })
+    }
+
+    render(){
+        const { clock, cup } = this.state
+
+        return (
+            <div>
+                <h1>{ clock }</h1>
+                <button onClick={() => this.changeCup()}>{cup}</button>
+            </div>
+        )
+    }
 }
 
 const rootElement = document.getElementById("root")
